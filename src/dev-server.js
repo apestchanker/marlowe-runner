f92370b0b464f912fbc51da0f6dbf49ac2e84721
Alex Pestchanker <apestchanker@gmail.com>
2024-05-20 16:11:51 +0000

    Adaptacion a local

--- src/dev-server.js
+++ src/dev-server.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const app = express()
 const port = 8080
 
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
+};
+
 app.use( "/", [ express.static("./public" ) ] );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+https.createServer(sslOptions, app).listen(port, () => {
+  console.log(`Example app listening on HTTPS port ${port}`);
+});
+//app.listen(port, () => {
+//  console.log(`Example app listening on port ${port}`)
+//})
